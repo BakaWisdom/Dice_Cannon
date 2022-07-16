fc46d93af817c8b5a011a8c773c5355d4b6abd37
BakaWisdom <42656515+BakaWisdom@users.noreply.github.com>
2022-07-16 12:27:28 -0600

    cannon rotation code
    basic setup for rotation code. definitely will need some touch up but good start.

--- Assets/scripts/MouseInputs.cs
+++ Assets/scripts/MouseInputs.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseInputs : MonoBehaviour
 {
+    Vector3 CannonPos;
+    float radIncline;
+    float maxRotationPerFrame;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //(x, y) init Cannon position for calculations place holder
+        CannonPos = new Vector2(5, 3);
+        radIncline = 45;
+        //could do a call to the cannon object for quick updates for what feels/looks right
+        int maxRotDegree = 5;
+        maxRotationPerFrame = maxRotDegree * MathF.PI / 180;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 angleVector = Input.mousePosition - CannonPos;
+        radIncline = CalcNewIncline(angleVector, radIncline);
+
+        //rotate cannon
+
+        //Left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Input.mousePosition);
+
+            //if in cannon scene fire cannon
+            if(false)
+            {
+                //fire cannon
+            }
+        }
+    }
+
+    private float CalcNewIncline(Vector2 angleVector, float oldRadIncline)
+    {
+        float newRadIncline = Mathf.Atan(angleVector.y / angleVector.x);
+
+        if(MathF.Abs(oldRadIncline - newRadIncline) >= maxRotationPerFrame)
+        {
+            if (oldRadIncline > newRadIncline)
+            {
+                newRadIncline = oldRadIncline - maxRotationPerFrame;
+            } 
+            else
+            {
+                newRadIncline = oldRadIncline + maxRotationPerFrame;
+            }
+        }
+
+        return newRadIncline;
     }
 }
